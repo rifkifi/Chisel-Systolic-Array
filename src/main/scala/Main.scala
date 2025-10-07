42fbf0efde58d1001e0e5c17449d9005f44e7ce1
rifkifi <rifkifi@gmail.com>
2025-10-07 15:33:20 +0200

    Complete Test for one Node

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,36 @@
 import chisel3._
 import chisel3.util._
 
-object SystolicArray extends Module {
+class SystolicArray(in_w: Int = 8, key_w: Int = 4) extends Module {
     val io = IO(new Bundle {
-    val keyIn = Input(UInt(4.W))
-    val valueIn = Input(UInt(8.W))
-    val keyOut = Output(UInt(4.W))
-    val valueOut = Output(UInt(8.W))
-    val cmd = Input(UInt(2.W))
-  })
+        val keyIn = Input(UInt(key_w.W))
+        val valueIn = Input(UInt(in_w.W))
+        val keyOut = Output(UInt(key_w.W))
+        val valueOut = Output(UInt(in_w.W))
+        val cmd = Input(UInt(2.W))
+    })
 
+    val regVal = RegInit(0.U(in_w.W))
+    val keyReg = RegInit(0.U(key_w.W))
+
+    // Default output assignments
+    io.keyOut := keyReg
+    io.valueOut := regVal
+
+    switch(io.cmd) {
+        is(0.U) {
+            regVal := 0.U
+            keyReg := 0.U
+            io.keyOut := 0.U
+            io.valueOut := 0.U
+        }
+        is(1.U) {
+            regVal := io.valueIn
+            keyReg := io.keyIn
+        }
+        is(2.U) {
+            io.keyOut := keyReg
+            io.valueOut := regVal
+        }
+    }
 }
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,20 +1,35 @@
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
 
-class UnitTest extends AnyFlatSpec with ChiselScalatestTester {
-  "Unit test" should "pass initialization" in {
+class SystolicArrayTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "SystolicArray"
+
+  it should "store and forward values correctly" in {
     test(new SystolicArray()) { dut =>
-      
-      // Initialize all signals
-      dut.io.keyIn(0).poke(0.U)
-      dut.io.valueIn(0).poke(0.U)
+      // Test reset
       dut.io.cmd.poke(0.U)
-      dut.clock.step(1)
+      dut.clock.step()
+      dut.io.valueOut.expect(0.U)
+      dut.io.keyOut.expect(0.U)
 
-      dut.io.keyOut(0).expect(0.U)
-      dut.io.valueOut(0).expect(0.U)
+      // Test storing values
+      dut.io.cmd.poke(1.U)
+      dut.io.valueIn.poke(42.U)
+      dut.io.keyIn.poke(5.U)
+      dut.clock.step()
+
+      // Test forwarding values
+      dut.io.cmd.poke(2.U)
+      dut.clock.step()
+      dut.io.valueOut.expect(42.U)
+      dut.io.keyOut.expect(5.U)
+
+      // Test holding values
+      dut.io.cmd.poke(3.U)  // Invalid command should hold values
+      dut.clock.step()
+      dut.io.valueOut.expect(42.U)
+      dut.io.keyOut.expect(5.U)
     }
   }
 }
